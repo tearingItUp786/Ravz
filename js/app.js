5ac3a5188fe2f75396b5093352e15aa53f8b6523
Taranveer Bains <Taranveer Bains>
2016-04-30 21:09:30 -0700

    So now I have it working with the toast grids and I need to start making it mobile friendly. I'll do that on Monda :)

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-$(window).scroll(function(e, data){
-  var windowScroll = $(this).scrollTop();
-  console.log(windowScroll);
-  $('.brush').css({
-    'transform': 'translate(0px, '+windowScroll/3+'%)'
-  });
-  $('#greeting').css({
-    'transform': 'translate(0px, '+windowScroll/1.5+'%)'
-  });
+$(window).scroll(function(e, data) {
+    var windowScroll = $(this).scrollTop();
+    console.log(windowScroll);
+    $('.brush').css({
+        'transform': 'translate(0px, ' + windowScroll / 3 + '%)'
+    });
+    $('#greeting').css({
+        'transform': 'translate(0px, ' + windowScroll / 1.5 + '%)'
+    });
 
-  if(windowScroll >= 300){
-    console.log("bro beans");
-    $('.navigation').addClass('tinted-nav');
-  }else{
-    $('.navigation').removeClass('tinted-nav');
-  }
+    if (windowScroll >= 300) {
+        console.log("bro beans");
+        $('nav').addClass('tinted-nav');
+    } else {
+        $('nav').removeClass('tinted-nav');
+    }
+});
+
+$(document).ready(function() {
+    var horizontalCenter = Math.floor(window.innerWidth / 2);
+    var verticalCener = Math.floor(window.innerHeight / 2);
+    console.log(horizontalCenter + " is horizontalCenter and " + verticalCener + " is vertial");
 });
