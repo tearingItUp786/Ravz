5a8c512c2eae88dc651220e5b6d90d616df2f47f
Taranveer Bains <Taranveer Bains>
2016-05-02 09:59:33 -0700

    Responsive navbar is up and running, need to get it tweeked for mobile. Right now it only works for medium breakpoint

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,3 +1,29 @@
+(function() {
+
+    "use strict";
+
+    var toggles = document.querySelectorAll(".c-hamburger");
+
+    for (var i = toggles.length - 1; i >= 0; i--) {
+        var toggle = toggles[i];
+        toggleHandler(toggle);
+    }
+
+    function toggleHandler(toggle) {
+        toggle.addEventListener("click", function(e) {
+            e.preventDefault();
+            if($(this).hasClass('is-active')){
+              $(this).removeClass('is-active');
+              $('.navigation').removeClass('open');
+            }else{
+              $(this).addClass('is-active');
+              $('.navigation').addClass('open');
+            }
+        });
+    }
+
+})();
+
 $(window).scroll(function(e, data) {
     var windowScroll = $(this).scrollTop();
     console.log(windowScroll);
@@ -5,7 +31,7 @@ $(window).scroll(function(e, data) {
         'transform': 'translate(0px, ' + windowScroll / 3 + '%)'
     });
     $('#greeting').css({
-        'transform': 'translate(0px, ' + windowScroll / 1.5 + '%)'
+        'transform': 'translate(0px, ' + windowScroll / 1 + '%)'
     });
 
     if (windowScroll >= 300) {
@@ -20,4 +46,5 @@ $(document).ready(function() {
     var horizontalCenter = Math.floor(window.innerWidth / 2);
     var verticalCener = Math.floor(window.innerHeight / 2);
     console.log(horizontalCenter + " is horizontalCenter and " + verticalCener + " is vertial");
+
 });
